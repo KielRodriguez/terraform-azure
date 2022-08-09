406a3474081d1002409f5ecfab7d2dd1fac73bc1
KielRodriguez <kr.rdz.20@gmail.com>
2022-08-08 23:34:31 -0500

    add default subnet

--- examples/private-endpoint/mssql/modules/vnet/main.tf
+++ examples/private-endpoint/mssql/modules/vnet/main.tf
@@ -8,9 +8,15 @@ resource "azurerm_virtual_network" "vnet" {
     resource_group_name = var.rg_name
 }
 
+resource "azurerm_subnet" "default" {
+    name = "default"
+    resource_group_name = var.rg_name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes = ["10.0.0.0/24"]
+}
 resource "azurerm_subnet" "mssql" {
     name = "mssql"
     resource_group_name = var.rg_name
     virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes = ["10.0.0.0/24"]
+    address_prefixes = ["10.0.1.0/24"]
 }
