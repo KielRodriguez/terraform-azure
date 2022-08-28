1984d19f76a754127e44d042976331d6dfd92022
KielRodriguez <kr.rdz.20@gmail.com>
2022-08-28 09:42:06 -0500

    fix error with disk size and private ip type

--- examples/private-endpoint/mssql/modules/vm/main.tf
+++ examples/private-endpoint/mssql/modules/vm/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_network_interface" "nic" {
     ip_configuration  {
         name =  "internal"
         subnet_id = var.subnet_id
-        private_ip_address_allocation = "Static"
+        private_ip_address_allocation = "Dynamic"
         public_ip_address_id = azurerm_public_ip.pip.id
     }
 }
@@ -37,7 +37,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
     os_disk {
         caching              = "ReadWrite"
         storage_account_type = "Standard_LRS"
-        disk_size_gb = 32
+        disk_size_gb = 127
     }
 
     source_image_reference {
