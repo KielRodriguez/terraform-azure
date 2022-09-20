4904f4e06080e464dc3cf5e834b025982f81b389
KielRodriguez <kr.rdz.20@gmail.com>
2022-09-19 22:17:47 -0500

    service endpoint subnet to Microsoft SQL

--- examples/private-endpoint/mssql/modules/vnet/main.tf
+++ examples/private-endpoint/mssql/modules/vnet/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_subnet" "mssql" {
     resource_group_name = var.rg_name
     virtual_network_name = azurerm_virtual_network.vnet.name
     address_prefixes = ["10.0.1.0/24"]
-    enforce_private_link_endpoint_network_policies = true
+    service_endpoints = ["Microsoft.Sql"]
 }
 
 
